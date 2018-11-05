9e706da66f83634132b9a4a773bf1c832f98b702
kintakCreator <41231005+kintakCreator@users.noreply.github.com>
2018-11-05 17:44:48 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
   string s[100000];
   int i,Max=0,nom=0,a[100000];
   for (i=1;i<=10;i++) cin >> s[i];
-  sort(s+1,s+1+10); reverse(s+1,s+1+10);
+  sort(s.begin(),s.end()); reverse(s.begin(),s.end());
   for (i=1;i<=10;i++) a[i]=s[i].length();
   for (i=1;i<=10;i++) 
     if (a[i]>Max) {Max=a[i]; nom=i;}
